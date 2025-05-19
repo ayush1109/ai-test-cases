a82ef742ba2c7ffdcd92d7cac9a4d9c86ef799a8
Ayush Garg <46477990+ayush1109@users.noreply.github.com>
2025-05-19 14:23:11 +0530

    changed port to 10000

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
 from app import app
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Use Railway's PORT
+    port = 10000  # Use Railway's PORT
     app.run(host="0.0.0.0", port=port)
