74ec562949da0a29a9019b98dbc8f30ef92a62b9
Ayush Garg <ayush.g1@turing.com>
2025-05-19 11:33:18 +0530

    added port in main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # run.py
 from app import app
+import os
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # Use Railway's PORT
+    app.run(host="0.0.0.0", port=port)         # Bind to public host
